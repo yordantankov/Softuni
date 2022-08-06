8fefbffb13a2c6ca92728666ed091d03ca2a9ff3
Yordan Tankov <94921040+yordantankov@users.noreply.github.com>
2022-08-06 17:11:09 +0300

    Add files via upload

---
+++ C# Fundamentals/Arrays - Exercise/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.ZigZag
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         int n = int.Parse(Console.ReadLine());
+         //zig-zag filled arrays
+          int [] arr1 = new int[n];
+          int [] arr2 = new int[n];
+
+            for (int row = 1; row <= n; row++)
+            {
+                int []currentRowData = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                int firstNumber = currentRowData[0];
+                int secondNumber = currentRowData[1];
+
+                if (row% 2 !=0)
+                {
+                    arr1[row - 1] = firstNumber;
+                    arr2[row - 1] = secondNumber;
+                }
+                else
+                {
+                    arr1[row - 1] = secondNumber;
+                    arr2[row - 1] = firstNumber;    
+                }
+               
+            }
+            Console.WriteLine(String.Join(" ", arr1));
+            Console.WriteLine(String.Join(" ", arr2));
+        }
+    }
+}
