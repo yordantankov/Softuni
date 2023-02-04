ffa4d4603358a5dd195a28c2438e6467ee64436f
Yordan Tankov <94921040+yordantankov@users.noreply.github.com>
2023-02-04 14:43:48 +0200

    07. Projects Creation

---
+++ PB_Python_Feb2023/first_steps_in_coding-LAB/07. Projects Creation.py
@@ -0,0 +1,7 @@
+
+arhitect_name = input()
+projects_qty = int(input())
+project_time = 3
+total_time = projects_qty * project_time
+
+print(f'The architect {arhitect_name} will need {total_time} hours to complete {projects_qty} project/s.')
