e71563fa99c276c0f0644d0a758566296866f011
Yordan Tankov <94921040+yordantankov@users.noreply.github.com>
2023-02-04 14:38:51 +0200

    04. Inches to Centimeters

--- PB_Python_Feb2023/first_steps_in_coding-LAB/main.py
+++ PB_Python_Feb2023/first_steps_in_coding-LAB/main.py
@@ -1,10 +1,3 @@
-print("1")
-print("2")
-print("3")
-print("4")
-print("5")
-print("6")
-print("7")
-print("8")
-print("9")
-print("10")
+inches = float(input())
+centimeters = inches * 2.54
+print (centimeters)
