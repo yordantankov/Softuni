df9c01d63b55b463c2d65923dbe9c69d6ff192fb
Yordan Tankov <94921040+yordantankov@users.noreply.github.com>
2023-02-10 13:52:05 +0200

    03. Time + 15 Minutes

---
+++ PB_Python_Feb2023/Conditional Statements - Exercise/03. Time + 15 Minutes.py
@@ -0,0 +1,22 @@
+hour = int(input())
+minutes = int (input())
+
+minutes += 15
+
+if minutes > 59 :
+
+    hour += 1
+    minutes = minutes - 60
+
+if hour == 24 :
+    hour = 0
+
+
+if minutes < 10 :
+     print (f"{hour}:0{minutes}")
+
+else:
+    print (f"{hour}:{minutes}")
+
+
+
