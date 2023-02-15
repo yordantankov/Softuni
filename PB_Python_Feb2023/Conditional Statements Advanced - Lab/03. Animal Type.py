bccd1c8648cbe74f360220e81b703f241adbaaba
Yordan Tankov <94921040+yordantankov@users.noreply.github.com>
2023-02-15 13:35:59 +0200

    03. Animal Type

---
+++ PB_Python_Feb2023/Conditional Statements Advanced - Lab/03. Animal Type.py
@@ -0,0 +1,14 @@
+animal = input()
+
+if animal == "dog" or animal =='crocodile' or animal == 'tortoise' or animal == 'snake' :
+    if animal == 'dog' :
+        print('mammal')
+    elif animal == 'crocodile':
+        print('reptile')
+    elif animal == 'tortoise':
+        print('reptile')
+    elif animal == 'snake':
+        print('reptile')
+
+else:
+    print('unknown')
