eec0bef41c2c367090200627fbd26ca21afb3472
Yordan Tankov <94921040+yordantankov@users.noreply.github.com>
2023-02-15 13:35:28 +0200

    02. Weekend or Working Day

---
+++ PB_Python_Feb2023/Conditional Statements Advanced - Lab/02. Weekend or Working Day.py
@@ -0,0 +1,20 @@
+day = input()
+
+if day == "Monday" or day == "Tuesday" or day == "Wednesday" or day == "Thursday" or day == "Friday" or day == "Saturday" or day == "Sunday" :
+    if day == "Monday" :
+        print('Working day')
+    elif day == "Tuesday":
+        print('Working day')
+    elif day == "Wednesday":
+        print('Working day')
+    elif day == "Thursday":
+        print('Working day')
+    elif day == "Friday":
+        print('Working day')
+    elif day == "Saturday":
+        print('Weekend')
+    elif day == "Sunday":
+        print('Weekend')
+
+else:
+    print('Error')
