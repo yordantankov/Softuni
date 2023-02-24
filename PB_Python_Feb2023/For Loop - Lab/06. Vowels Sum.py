526cfd3bc8bb2687a019719334123325c628b7b7
Yordan Tankov <94921040+yordantankov@users.noreply.github.com>
2023-02-25 00:40:49 +0200

    06. Vowels Sum

---
+++ PB_Python_Feb2023/For Loop - Lab/06. Vowels Sum.py
@@ -0,0 +1,16 @@
+text = input()
+sum = 0
+
+for char in text:
+    if char == "a":
+        sum += 1
+    elif char == "e":
+        sum += 2
+    elif char == "i":
+        sum += 3
+    elif char == "o":
+        sum += 4
+    elif char == "u":
+        sum += 5
+
+print(f"{sum}")
