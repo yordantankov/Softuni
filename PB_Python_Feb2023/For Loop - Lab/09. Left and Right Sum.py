76a94e375e77ea2c5e13bd0ffa6638d3a2f96673
Yordan Tankov <94921040+yordantankov@users.noreply.github.com>
2023-02-26 12:02:38 +0200

    09. Left and Right Sum

---
+++ PB_Python_Feb2023/For Loop - Lab/09. Left and Right Sum.py
@@ -0,0 +1,17 @@
+n = int(input())
+left_sum = 0
+right_sum = 0
+
+for i in range (0, n):
+    left_sum = left_sum + int(input())
+
+for i in range (0, n):
+    right_sum = right_sum + int(input())
+
+if left_sum == right_sum:
+    print(f'Yes, sum = {left_sum}')
+else:
+    diff = abs(left_sum - right_sum)
+    print(f'No, diff = {diff}')
+
+
