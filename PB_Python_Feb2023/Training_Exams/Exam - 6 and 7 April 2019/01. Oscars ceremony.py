8005a22f61479fffd31977b94450c5f1727b6db1
Yordan Tankov <94921040+yordantankov@users.noreply.github.com>
2023-03-15 20:01:24 +0200

    Delete PB_Python_Feb2023/Training_Exams/Exam - 6 and 7 April 2019 directory

--- PB_Python_Feb2023/Training_Exams/Exam - 6 and 7 April 2019/01. Oscars ceremony.py
+++
@@ -1,9 +0,0 @@
-rent = int(input())
-
-statuetki = rent - (rent * 0.30)
-ketaring = statuetki -(statuetki * 0.15)
-music = ketaring / 2
-
-total = rent + statuetki + ketaring + music
-
-print(f'{total :.2f}')
