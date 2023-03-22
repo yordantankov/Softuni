ce216c402d8ebbca7bee003203662cf6d7d0913d
Yordan Tankov <94921040+yordantankov@users.noreply.github.com>
2023-03-23 00:45:51 +0200

    03. Fitness Card

---
+++ PB_Python_Feb2023/Training_for_Exam/Exam - 28 and 29 March 2020/03. Fitness Card/03. Fitness Card.py
@@ -0,0 +1,40 @@
+budjet = float(input())
+gender = input()[0]
+age = int(input())
+sport = input()
+price = 0
+if gender == "m":
+    if sport == "Gym":
+        price = 42
+    elif sport == "Boxing":
+        price = 41
+    elif sport == "Yoga":
+        price = 45
+    elif sport == "Zumba":
+        price = 34
+    elif sport == "Dances":
+        price = 51
+    elif sport == "Pilates":
+        price = 39
+elif gender == "f":
+    if sport == "Gym":
+        price = 35
+    elif sport == "Boxing":
+        price = 37
+    elif sport == "Yoga":
+        price = 42
+    elif sport == "Zumba":
+        price = 31
+    elif sport == "Dances":
+        price = 53
+    elif sport == "Pilates":
+        price = 37
+
+if age <= 19:
+    price -= price * 0.20
+
+if budjet >= price:
+    print(f'You purchased a 1 month pass for {sport}.')
+else:
+    diff = price - budjet
+    print(f"You don't have enough money! You need ${diff :.2f} more.")
