ea2dda5e8fb17ac8e3678ebd6c5fd1a42b425e33
Yordan Tankov <94921040+yordantankov@users.noreply.github.com>
2023-06-02 18:01:41 +0300

    5. Special Numbers

---
+++ Fundamentals_Python_May_2023/Data Types and Variables - Lab/5. Special Numbers.py
@@ -0,0 +1,14 @@
+num = int(input())
+
+for i in range(1 , num + 1):
+    sum_of_digits = 0
+    digits = i
+
+    while digits > 0:
+        sum_of_digits += digits % 10
+        digits = int(digits / 10)
+
+    if sum_of_digits  == 5 or sum_of_digits == 7 or sum_of_digits == 11:
+        print(f'{i} -> True')
+    else:
+        print(f'{i} -> False')
