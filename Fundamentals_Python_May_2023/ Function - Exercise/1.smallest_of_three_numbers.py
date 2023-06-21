dfa7e9957711bfa53ab0d0451136850710687d96
Yordan Tankov <94921040+yordantankov@users.noreply.github.com>
2023-06-21 19:57:55 +0300

    1.smallest_of_three_numbers

---
+++ Fundamentals_Python_May_2023/ Function - Exercise/1.smallest_of_three_numbers.py
@@ -0,0 +1,10 @@
+def smallest_num(numbers):
+    smalest = min(nums)
+    return smalest
+
+
+first_num = int(input())
+second_num = int(input())
+third_num = int(input())
+nums = [first_num, second_num,third_num ]
+print(smallest_num(nums))
