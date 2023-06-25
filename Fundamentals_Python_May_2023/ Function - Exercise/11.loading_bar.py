b31efd85755b6ec47888d11be194e44236e850cf
Yordan Tankov <94921040+yordantankov@users.noreply.github.com>
2023-06-25 19:29:42 +0300

    11.loading_bar

---
+++ Fundamentals_Python_May_2023/ Function - Exercise/11.loading_bar.py
@@ -0,0 +1,9 @@
+def loading_bar (num):
+    if num == 100:
+        return f'100% Complete! \n[%%%%%%%%%%]'
+    else:
+        return f"{num}% [{'%' * (num // 10)}{'.' * (10 - num // 10)}]\nStill loading..."
+
+number = int(input())
+
+print(loading_bar(number))
