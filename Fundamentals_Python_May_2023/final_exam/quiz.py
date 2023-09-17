b499caf268908946f23e11b8c075dc72743acf44
Yordan Tankov <94921040+yordantankov@users.noreply.github.com>
2023-09-17 15:22:23 +0300

    quiz

---
+++ Fundamentals_Python_May_2023/final_exam/quiz.py
@@ -0,0 +1,26 @@
+# a = 5
+# b = a > 2
+# if not a:
+#     print(a)
+# if b:
+#     print(b)
+
+# for i in range(10, 3, -2):
+#     print(i, end=" ")
+#
+
+# def print_text(text):
+#     print("I love" + text)
+# print_text("Python")
+
+# x = lambda a: a ** 3
+# print(x(2))
+
+# name = "George"
+# name[2] = 'm'
+# print(name[2])
+
+a = 10
+b = 20
+if b > a:
+   print("b is gre")
