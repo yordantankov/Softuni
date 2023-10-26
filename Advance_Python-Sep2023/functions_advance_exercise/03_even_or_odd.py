699ccff558810061dffba796fad7bff2246d644e
Yordan Tankov <94921040+yordantankov@users.noreply.github.com>
2023-10-26 14:41:56 +0300

    03_even_or_odd

---
+++ Advance_Python-Sep2023/functions_advance_exercise/03_even_or_odd.py
@@ -0,0 +1,11 @@
+def even_odd(*args):
+    command = args[-1]
+    if command == "even":
+        return list(filter(lambda x: x % 2 == 0, args[:-1]))
+    elif command == "odd":
+        return list(filter(lambda x: x % 2 != 0, args[:-1]))
+
+
+print(even_odd(1, 2, 3, 4, 5, 6, "even"))
+print()
+print(even_odd(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "odd"))
