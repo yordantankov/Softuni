1f41f6fad429306daf2a3d0210499634e88cdcd0
Yordan Tankov <94921040+yordantankov@users.noreply.github.com>
2023-11-06 15:51:51 +0200

    02_matrix_modification

---
+++ Advance_Python-Sep2023/multidimensional_list_exercise2/02_matrix_modification.py
@@ -0,0 +1,24 @@
+rows = int(input())
+matrix = [[int(x) for x in input().split()] for _ in range(rows)]
+
+while True:
+    command = input().split()
+
+    if command[0] == "END":
+        break
+
+    row = int(command[1])
+    col = int(command[2])
+    value = int(command[3])
+
+    if row not in range(rows) or col not in range(rows):
+        print("Invalid coordinates")
+        continue
+
+    if command[0] == "Add":
+        matrix[row][col] += value
+    elif command[0] == "Subtract":
+        matrix[row][col] -= value
+
+for row in matrix:
+    print(*row, sep=" ")
