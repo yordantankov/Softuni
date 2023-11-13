e66fae4b2cb31e9ed240d27b79f1d672de699386
Yordan Tankov <94921040+yordantankov@users.noreply.github.com>
2023-11-14 00:05:25 +0200

    01_numbers

---
+++ Advance_Python-Sep2023/stacks_queues_tuples_and_sets_exercise/01_numbers.py
@@ -0,0 +1,22 @@
+first_set = set(int(x) for x in input().split())
+second_set = set(int(x) for x in input().split())
+
+n = int(input())
+
+for i in range(n):
+    line = input().split()
+    command = line[0] + " " + line[1]
+    numbers = [int(num) for num in line[2:]]
+    if command == "Add First":
+        first_set.update(numbers)
+    elif command == "Add Second":
+        second_set.update(numbers)
+    elif command == "Remove First":
+        first_set.difference_update(numbers)
+    elif command == "Remove Second":
+        second_set.difference_update(numbers)
+    elif command == "Check Subset":
+        print(first_set.issubset(second_set) or second_set.issubset(first_set))
+
+print(*sorted(first_set), sep=", ")
+print(*sorted(second_set), sep=", ")
