a0340fb2a45421bb9f212b7bd247e5372f8f0d8d
Yordan Tankov <94921040+yordantankov@users.noreply.github.com>
2023-12-26 01:41:51 +0200

    product_repository

---
+++ Python_OOP/inheritance_exercise/inheritance_exercise/05_shop/product_repository.py
@@ -0,0 +1,22 @@
+from project.product import Product
+
+
+class ProductRepository:
+    def __init__(self):
+        self.products = []
+
+    def add(self, product: Product):
+        self.products.append(product)
+
+    def find(self, product_name):
+        for product in self.products:
+            if product.name == product_name:
+                return product
+
+    def remove(self, product_name):
+        product = self.find(product_name)
+        if product:
+            self.products.remove(product)
+
+    def __repr__(self):
+        return '\n'.join(f"{product.name}: {product.quantity}" for product in self.products)
