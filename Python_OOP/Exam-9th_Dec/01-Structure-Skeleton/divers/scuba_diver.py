01f8aa0acdaa190ca0a27fd26cd89a5de1b9f405
Yordan Tankov <94921040+yordantankov@users.noreply.github.com>
2024-02-07 02:15:33 +0200

    scuba_diver

---
+++ Python_OOP/Exam-9th_Dec/01-Structure-Skeleton/divers/scuba_diver.py
@@ -0,0 +1,20 @@
+from project.divers.base_diver import BaseDiver
+import math
+
+
+class ScubaDiver(BaseDiver):
+
+    def __init__(self,name:str):
+        super().__init__(name,540)
+        self.capacity = 540
+
+    def miss(self,time_to_catch:int):
+        self.oxygen_level -= 0.3*time_to_catch
+        if self.oxygen_level<0 or self.oxygen_level == 0:
+            self.has_health_issue = True
+            self.oxygen_level = 0
+        self.oxygen_level = round(self.oxygen_level)
+
+    def renew_oxy(self):
+        self.oxygen_level = 540
+
