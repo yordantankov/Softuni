7b4358741931589ac4074f4f632fcdc00b336a33
Yordan Tankov <94921040+yordantankov@users.noreply.github.com>
2024-02-08 18:36:30 +0200

    predatory_fish

---
+++ Python_OOP/Exam-9th_Dec/01-Structure-Skeleton/fish/predatory_fish.py
@@ -0,0 +1,11 @@
+from project.fish.base_fish import BaseFish
+
+
+class PredatoryFish(BaseFish):
+
+    def __init__(self,name:str,points:int):
+        super().__init__(name,points,90)
+
+    def fish_details(self):
+        return f"{self.__class__.__name__}: {self.name} [Points: {self.points}, Time to Catch: {self.time_to_catch} seconds]"
+
